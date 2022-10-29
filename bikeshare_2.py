7792de1540381d50a6c6c255ee030d1f451386f2
Danish Arshad <darshad007@gmail.com>
2022-10-29 19:03:48 +0530

    Changed doc strings of user stats function

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -148,7 +148,7 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Displays statistics such as different type of users on bikeshare users."""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
