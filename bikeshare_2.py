ceab7057887798e01852780c395fbe9b687f8926
Danish Arshad <darshad007@gmail.com>
2022-10-29 19:05:32 +0530

    Changed doc strings of station stats function

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -108,7 +108,7 @@ def time_stats(df):
 
 
 def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
+    """Displays statistics on the most popular start and end stations and trip."""
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
